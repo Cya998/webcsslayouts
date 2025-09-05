cdd10cbf57e36412d9efb975b322f242121e4fba
Cya998 <luxie35@gmail.com>
2025-09-05 12:31:51 +0300

    Nachos

---
+++ OneDrive/Desktop/Answers.py/Week2.py
@@ -0,0 +1,12 @@
+my_list = [5,25,35,45]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(2,15)
+Zees = [50,60,70]
+my_list.extend(Zees)
+my_list.remove(70)
+my_list.sort()
+print (my_list)
+print (my_list.index(30))
